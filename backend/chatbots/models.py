d38ad8b9a6cf5df6dd15d08f5ed9fe60b7a0d4cc
AV2001 <anirudhvadlamani2001@gmail.com>
2024-10-19 13:31:18 +0400

    create Chatbot model

--- backend/chatbots/models.py
+++ backend/chatbots/models.py
@@ -1,3 +1,17 @@
+import uuid
 from django.db import models
+from organizations.models import Organization
 
-# Create your models here.
+
+class Chatbot(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    initial_message = models.TextField()
+    color = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    organization = models.ForeignKey(
+        Organization, on_delete=models.CASCADE, related_name="chatbots"
+    )
+
+    class Meta:
+        db_table = "chatbots"
