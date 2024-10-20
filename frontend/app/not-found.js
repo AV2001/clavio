a054dff1a1b67d77352dad0fc45e478156473eed
AV2001 <anirudhvadlamani2001@gmail.com>
2024-10-20 22:01:22 +0400

    initial commit

---
+++ frontend/app/not-found.js
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import { Button } from '@/app/_components/shadcn/button';
+
+export default function NotFound() {
+  return (
+    
+    <main className='flex-grow px-6 mx-auto max-w-7xl min-h-screen flex flex-col items-center justify-center'>
+      <h1 className='text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl mb-4'>
+        404
+      </h1>
+      <p className='text-xl text-muted-foreground mb-8'>
+        Oops! The page you're looking for doesn't exist.
+      </p>
+      <Button className='bg-primary-800 hover:bg-primary-900 text-white'>
+        <Link href='/'>Back to Home</Link>
+      </Button>
+    </main>
+  );
+}
