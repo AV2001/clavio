ec44679611674b6d9826463ebbe94b795a5b9f2e
jay-ry <jayryan267@gmail.com>
2024-10-21 23:42:01 +0400

    ceated ChatbotQueryView and formatted ChatbotView

--- backend/chatbots/views.py
+++ backend/chatbots/views.py
@@ -1,7 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
-from .train import train_chatbot
+from .train import train_chatbot, get_chatbot_response
+import os
+from django.conf import settings
 
 
 class ChatbotView(APIView):
@@ -12,9 +14,33 @@ class ChatbotView(APIView):
         name = request.data.get("name")
         files = request.FILES.getlist("files")
 
-        # pass the name and files to the train.py file
-        file = train_chatbot(name, files)
-        # return the response
+        # Get the file paths for the uploaded files
+        if files:
+            file_paths = []
+            for file in files:
+                file_path = os.path.join(settings.MEDIA_ROOT, file.name)
+                with open(file_path, "wb+") as destination:
+                    for chunk in file.chunks():
+                        destination.write(chunk)
+                file_paths.append(file_path)
+        else:
+            return Response({"error": "No files uploaded"}, status=400)
 
-        # print(files)
-        return Response({"message": "Hello, World!"})
+        response = train_chatbot(file_paths)
+
+        # return the response from train_chatbot
+        return Response(
+            {
+                "message": response["message"],
+            },
+            status=response["status"],
+        )
+
+
+class ChatbotQueryView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        question = request.data.get("question")
+        response = get_chatbot_response(question)
+        return Response({"response": response})
