32efa9e5254d148d776ce24c7613c25cdcc7dde7
jay-ry <jayryan267@gmail.com>
2024-10-21 23:43:08 +0400

    added response/ endpoint

--- backend/chatbots/urls.py
+++ backend/chatbots/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import ChatbotView
+from .views import ChatbotView, ChatbotQueryView
 
-urlpatterns = [path("", ChatbotView.as_view(), name="chatbots")]
+urlpatterns = [
+    path("", ChatbotView.as_view(), name="chatbots"),
+    path("response/", ChatbotQueryView.as_view(), name="chatbot_response"),
+]
