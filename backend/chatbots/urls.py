dee84a8262b05bac9f888bf6d5d2a3e58b528dd8
AV2001 <anirudhvadlamani2001@gmail.com>
2024-10-25 23:36:24 +0400

    create endpoint to generate embedded chatbot

--- backend/chatbots/urls.py
+++ backend/chatbots/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ChatbotView
+from .views import ChatbotView, ChatbotEmbedScriptView
 
 urlpatterns = [
     path("", ChatbotView.as_view(), name="chatbots"),
+    path('embed/<uuid:embed_id>.js', ChatbotEmbedScriptView.as_view(), name='chatbot_embed_script'),
 ]
