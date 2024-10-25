f75bf7e8f3d8e4f579cd7482f233edf34655f09c
AV2001 <anirudhvadlamani2001@gmail.com>
2024-10-25 10:22:12 +0400

    add more chatbot customization fields in the Chatbot model

--- backend/chatbots/models.py
+++ backend/chatbots/models.py
@@ -5,14 +5,20 @@ from organizations.models import Organization
 
 class Chatbot(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=255)
-    initial_message = models.TextField()
-    color = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
     organization = models.ForeignKey(
         Organization, on_delete=models.CASCADE, related_name="chatbots"
     )
     embed_id = models.UUIDField(unique=True, null=True, editable=False)
+    name = models.CharField(max_length=255)
+    initial_message = models.TextField()
+    image = models.URLField(null=True, blank=True)
+    primary_color = models.CharField(max_length=10, null=True, blank=True)
+    secondary_color = models.CharField(max_length=10, null=True, blank=True)
+    chatbot_border_radius = models.IntegerField(null=True, blank=True)
+    font_size = models.IntegerField(null=True, blank=True)
+    widget_color = models.CharField(max_length=10, null=True, blank=True)
+    widget_border_radius = models.IntegerField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = "chatbots"
