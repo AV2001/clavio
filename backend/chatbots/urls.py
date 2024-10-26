006dbda3997639ce49d1e3b4d16729c5c79d649d
AV2001 <anirudhvadlamani2001@gmail.com>
2024-10-26 21:56:40 +0400

    format file

--- backend/chatbots/urls.py
+++ backend/chatbots/urls.py
@@ -3,5 +3,9 @@ from .views import ChatbotView, ChatbotEmbedScriptView
 
 urlpatterns = [
     path("", ChatbotView.as_view(), name="chatbots"),
-    path('embed/<uuid:embed_id>.js', ChatbotEmbedScriptView.as_view(), name='chatbot_embed_script'),
+    path(
+        "embed/<uuid:embed_id>.js",
+        ChatbotEmbedScriptView.as_view(),
+        name="chatbot_embed_script",
+    ),
 ]
