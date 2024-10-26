41c345722a0fb2fc04041981844404775704e476
AV2001 <anirudhvadlamani2001@gmail.com>
2024-10-26 21:54:46 +0400

    remove email in Organization

--- backend/organizations/models.py
+++ backend/organizations/models.py
@@ -5,7 +5,6 @@ from django.db import models
 class Organization(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
