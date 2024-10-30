82da758180719d997109b9e25a5dc480f1198ff7
jay-ry <jayryan267@gmail.com>
2024-10-31 01:11:53 +0400

    added auth url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/chatbots/", include("chatbots.urls")),
+    path("api/auth/", include("auth.urls")),
     path("api/users/", include("users.urls")),
     path("api/organizations/", include("organizations.urls")),
+    path("api/chatbots/", include("chatbots.urls")),
 ]
