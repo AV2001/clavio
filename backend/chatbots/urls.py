5857c79f40cddc209f7400529d9d962697a33905
AV2001 <anirudhvadlamani2001@gmail.com>
2024-11-01 19:58:11 +0400

    create endpoint for ChatbotDetailView

--- backend/chatbots/urls.py
+++ backend/chatbots/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import ChatbotView, ChatbotEmbedScriptView
+from .views import ChatbotView, ChatbotDetailView, ChatbotEmbedScriptView
 
 urlpatterns = [
     path("", ChatbotView.as_view(), name="chatbots"),
+    path("<uuid:chatbot_id>/", ChatbotDetailView.as_view(), name="chatbot_detail"),
     path(
         "embed/<uuid:embed_id>.js",
         ChatbotEmbedScriptView.as_view(),
