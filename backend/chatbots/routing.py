e0e43f6e1b3cfac0732c2ce1fa883629df811a6a
AV2001 <anirudhvadlamani2001@gmail.com>
2024-11-01 20:00:44 +0400

    create websocket endpoint for ChatbotStatusConsumer

--- backend/chatbots/routing.py
+++ backend/chatbots/routing.py
@@ -1,6 +1,9 @@
 from django.urls import re_path
-from . import consumers
+from .consumers import ChatConsumer, ChatbotStatusConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/(?P<embed_id>[^/]+)/$", consumers.ChatConsumer.as_asgi()),
+    re_path(r"ws/chat/(?P<embed_id>[^/]+)/$", ChatConsumer.as_asgi()),
+    re_path(
+        r"ws/chatbot/(?P<chatbot_id>[^/]+)/status/$", ChatbotStatusConsumer.as_asgi()
+    ),
 ]
