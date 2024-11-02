191ec2cf639f19f05555fc2bf05b2ccee372d232
AV2001 <anirudhvadlamani2001@gmail.com>
2024-11-02 22:56:55 +0400

    use settings_module in place of backend.settings when setting the default environment

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,10 +1,11 @@
 import os
+from backend.settings import settings_module
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chatbots.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 application = ProtocolTypeRouter(
     {
