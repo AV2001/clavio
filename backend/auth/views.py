a39a5d83d827877283ed9de8a0ddb211dce2fe4f
AV2001 <anirudhvadlamani2001@gmail.com>
2024-12-01 19:02:13 +0400

    modify the post method in LoginView to return response including tokens and user information

--- backend/auth/views.py
+++ backend/auth/views.py
@@ -2,10 +2,10 @@ from django.contrib.auth import get_user_model
 from django.middleware import csrf
 from django.contrib.auth import authenticate
 from rest_framework.views import APIView
-from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import AllowAny
+
 
 User = get_user_model()
 
@@ -19,34 +19,39 @@ def get_tokens_for_user(user):
 
 
 class LoginView(APIView):
-    authentication_classes = [JWTAuthentication]
     permission_classes = [AllowAny]
 
     def post(self, request, format=None):
-        response = Response()
         email = request.data.get("email")
-        user = authenticate(email=email)
+        password = request.data.get("password")
 
-        if user is not None:
-            if user.is_active:
-                tokens = get_tokens_for_user(user)
-                csrf_token = csrf.get_token(request)
-
-                response.data = {
-                    "message": "Login successfully",
-                    "tokens": {
-                        **tokens,
-                        "csrf": csrf_token,
-                    },
-                }
-                return response
-            else:
-                return Response(
-                    {"error": "This account is not active!!"},
-                    status=404,
-                )
-        else:
+        if not email or not password:
             return Response(
-                {"error": "Invalid username or password!!"},
-                status=404,
+                {"error": "Please provide both email and password"},
+                status=400,
             )
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is None:
+            return Response(
+                {"error": "Invalid email or password"},
+                status=401,
+            )
+
+        tokens = get_tokens_for_user(user)
+        csrf_token = csrf.get_token(request)
+
+        return Response(
+            {
+                "message": "Login successful",
+                "tokens": {
+                    **tokens,
+                    "csrf": csrf_token,
+                },
+                "user": user.id,
+                "fullName": user.full_name,
+                "email": user.email,
+                "isAdmin": user.is_admin,
+            }
+        )
