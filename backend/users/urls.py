db09297a25b69532035872df7ee31429c2461a72
AV2001 <anirudhvadlamani2001@gmail.com>
2024-12-01 19:04:44 +0400

    add endpoint to get details of a user

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import UserView
+from .views import UserView, UserDetailView
 
-urlpatterns = [path("", UserView.as_view(), name="create_user")]
+urlpatterns = [
+    path("", UserView.as_view(), name="create_user"),
+    path("get/", UserDetailView.as_view(), name="get_user"),
+]
