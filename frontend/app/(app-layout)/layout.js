24a23f97089cb37b5f40432d8cd4d01143620f98
AV2001 <anirudhvadlamani2001@gmail.com>
2024-12-01 18:53:04 +0400

    add SessionProvider

--- frontend/app/(app-layout)/layout.js
+++ frontend/app/(app-layout)/layout.js
@@ -1,18 +1,25 @@
+import { auth } from '@/auth';
 import ReactQueryProvider from '@/app/_utils/providers/ReactQueryProvider';
 import ToastProvider from '@/app/_utils/providers/ToastProvider';
 import Header from '@/app/_components/Header';
 import Sidebar from '@/app/_components/Sidebar';
+import { SessionProvider } from 'next-auth/react';
 
-export default function AppLayout({ children }) {
+export default async function AppLayout({ children }) {
+  const session = await auth();
+
   return (
-    <div className='grid grid-cols-[14rem_1fr] grid-rows-[auto_1fr] min-h-screen'>
-      <Header />
-      <Sidebar />
-      <ReactQueryProvider>
-        <ToastProvider>
-          <main className='p-8 rounded-lg bg-primary-100'>{children}</main>
-        </ToastProvider>
-      </ReactQueryProvider>
-    </div>
+    <SessionProvider session={session}>
+      <div className='grid grid-cols-[14rem_1fr] grid-rows-[auto_1fr] min-h-screen'>
+        <Header />
+        <Sidebar />
+        <ReactQueryProvider>
+          <ToastProvider>
+            <main className='p-8 rounded-lg bg-primary-100'>{children}</main>
+          </ToastProvider>
+          b
+        </ReactQueryProvider>
+      </div>
+    </SessionProvider>
   );
 }
