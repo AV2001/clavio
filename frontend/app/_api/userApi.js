7188801017e2137a38c743656f41df8499138b46
AV2001 <anirudhvadlamani2001@gmail.com>
2024-12-02 16:51:33 +0400

    remove checkUserExists function

--- frontend/app/_api/userApi.js
+++ frontend/app/_api/userApi.js
@@ -1,19 +1,5 @@
 import axiosInstance from './axiosInstance';
 
-// Use only during initial sign-in (no auth required)
-export async function checkUserExists({ email }) {
-  try {
-    const response = await axiosInstance.get(`/users/exists/?email=${email}`);
-    return response.data;
-  } catch (error) {
-    if (error?.response?.status === 404) {
-      return { data: null };
-    }
-    // console.error('Error checking user:', error);
-    throw error;
-  }
-}
-
 // Used for authenticated requests
 export async function getUser({ email }) {
   try {
