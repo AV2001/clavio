14c51d82f3136c2068441ac5558977be338e3e9a
AV2001 <anirudhvadlamani2001@gmail.com>
2024-12-03 16:38:06 +0400

    add the authorized callback

--- frontend/auth.js
+++ frontend/auth.js
@@ -41,6 +41,9 @@ export const { auth, signIn, signOut, handlers } = NextAuth({
     }),
   ],
   callbacks: {
+    authorized({ auth, request }) {
+      return !!auth?.user;
+    },
     async jwt({ token, user }) {
       if (user) {
         // Initial sign in - add user data to token
