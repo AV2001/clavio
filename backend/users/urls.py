edd8e41e725a3f3f1d214163b39b541e7ebb991b
AV2001 <anirudhvadlamani2001@gmail.com>
2024-12-04 16:12:43 +0400

    add endpoint to invite team member

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserView, UserDetailView
+from .views import UserView, UserDetailView, InviteTeamMemberView
 
 urlpatterns = [
     path("", UserView.as_view(), name="create_user"),
     path("get/", UserDetailView.as_view(), name="get_user"),
+    path("invite/", InviteTeamMemberView.as_view(), name="invite_team_member"),
 ]
