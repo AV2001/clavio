d897e5380d5fe703015313a5bc7a8757f60c9b4e
AV2001 <anirudhvadlamani2001@gmail.com>
2024-12-07 12:28:04 +0400

    may CustomTokenRefreshView to token/refresh/

--- backend/auth/urls.py
+++ backend/auth/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import LoginView
-from rest_framework_simplejwt.views import TokenRefreshView
+from .views import LoginView, CustomTokenRefreshView
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/refresh/", CustomTokenRefreshView.as_view(), name="token_refresh"),
 ]
