2f2379d55c499ce39e8d5e54a6f07f3ec5ee3baa
AV2001 <anirudhvadlamani2001@gmail.com>
2024-12-08 13:37:02 +0400

    replace get/ with user_id to map to UserDetailView

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,6 +3,6 @@ from .views import UserView, UserDetailView, InviteTeamMemberView
 
 urlpatterns = [
     path("", UserView.as_view(), name="create_user"),
-    path("get/", UserDetailView.as_view(), name="get_user"),
+    path("<uuid:user_id>/", UserDetailView.as_view(), name="user_detail"),
     path("invite/", InviteTeamMemberView.as_view(), name="invite_team_member"),
 ]
