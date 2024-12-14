66d898bcbb6fc6fb6e46e4ec9f8b4edd63fb78d6
jay-ry <jayryan267@gmail.com>
2024-12-14 15:15:45 +0400

    added num_chatbot column

--- backend/organizations/models.py
+++ backend/organizations/models.py
@@ -6,6 +6,7 @@ class Organization(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
+    num_chatbots = models.IntegerField(default=0)
 
     class Meta:
         db_table = "organizations"
