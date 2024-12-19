31fa7a5111f3898da1b0ae0eebf5f9b7d6f65bcc
jay-ry <jayryan267@gmail.com>
2024-12-19 18:39:00 +0400

    added url router for SSE

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -3,15 +3,27 @@ from backend.settings import settings_module
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chatbots.routing
+from chatbots.routing import websocket_urlpatterns
+import django_eventstream.urls as eventstream_urls
+from django.urls import path, re_path
+import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
+django.setup()
+django_asgi_app = get_asgi_application()
+
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
-        "websocket": AuthMiddlewareStack(
-            URLRouter(chatbots.routing.websocket_urlpatterns)
+        "http": URLRouter(
+            [
+                path(
+                    "events/",
+                    AuthMiddlewareStack(URLRouter(eventstream_urls.urlpatterns)),
+                ),
+                re_path(r"", django_asgi_app),
+            ]
         ),
+        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
     }
 )
