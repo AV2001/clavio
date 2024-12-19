3aeadb9e4ebc0a75297af780e030fdb5fd28431b
jay-ry <jayryan267@gmail.com>
2024-12-19 18:26:12 +0400

    installed stripe and django_eventstream modules

--- backend/users/utils.py
+++ backend/users/utils.py
@@ -6,10 +6,10 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def send_invite_email(from_email, to_email, organization_name, invite_token):
+def send_invite_email(from_email, to_email, organization_name):
     try:
         resend.api_key = RESEND_API_KEY
-        signup_url = f"{FRONTEND_URL}/signup?type=invite&token={invite_token}"
+        signup_url = f"{FRONTEND_URL}/signup?type=invite"
 
         params: resend.Emails.SendParams = {
             "from": from_email,
