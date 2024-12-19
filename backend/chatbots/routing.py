9fc41e4670650ba4a7ae5ebc8e0c8bff20d48a2e
jay-ry <jayryan267@gmail.com>
2024-12-19 18:39:54 +0400

    renamed chatconsumer to externalchatconsumer

--- backend/chatbots/routing.py
+++ backend/chatbots/routing.py
@@ -1,8 +1,8 @@
 from django.urls import re_path
-from .consumers import ChatConsumer, ChatbotStatusConsumer
+from .consumers import ExternalChatConsumer, ChatbotStatusConsumer
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/(?P<embed_id>[^/]+)/$", ChatConsumer.as_asgi()),
+    re_path(r"ws/chat/(?P<embed_id>[^/]+)/$", ExternalChatConsumer.as_asgi()),
     re_path(
         r"ws/chatbot/(?P<chatbot_id>[^/]+)/status/$", ChatbotStatusConsumer.as_asgi()
     ),
